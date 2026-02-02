cb7e73fb5a36695347065499b6e949772d528af2
Christine <christineee222@proton.me>
2026-02-02 10:52:10 -0600

    Add essence_numen relationship to Events

--- app/Models/Events.php
+++ app/Models/Events.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\EssenceNumen;
 
 class Events extends Model
 {
-    //
+    use HasFactory;
+
+    public function essenceNumen()
+    {
+        return $this->belongsTo(EssenceNumen::class);
+    }
 }
